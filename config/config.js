0457e6daaf3fe9968f930fbdcb147fae6fe5a597
dominic <dominic.b@mrinq.com>
2020-06-11 12:43:44 +0530

    added config info

--- config/config.js
+++ config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     }
   },
   test: {
-    username: "root",
-    password: null,
-    database: "spintly_lighting_test",
-    host: "127.0.0.1",
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: "home-lighting-db.coyvxgfrewsk.ap-south-1.rds.amazonaws.com",
     dialect: "postgres",
     pool: {
       max: 2,
